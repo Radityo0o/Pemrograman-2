262c3a71b7b36d2ba3bf2990197a5304a388857c
Radityo0o <agustinus.20246002@student.atmi.ac.id>
2025-08-10 00:25:08 +0700

    Update main.cpp

--- GroupProject/main.cpp
+++ GroupProject/main.cpp
@@ -1,1 +1,58 @@
+#include "kelas.h"
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <iomanip>
+using namespace std;
 
+int main()
+{
+    Kelass kelas;
+
+    string passwordLogin, opsi;
+
+    cout << "Aplikasi Pemesan Hotel" << endl;
+    while (true)
+    {
+        cout << "Password: ";
+        cin >> passwordLogin;
+        if (passwordLogin == "admin123")
+        {
+            cout << "Login berhasil!" << endl;
+            break;
+        } 
+        else
+        {
+            cout << "Password salah, coba lagi!" << endl;
+        } 
+    }
+        
+awal:
+    while (true)
+    {
+    kelas.clearScreen();
+    kelas.headerUtama();
+    kelas.menuUtama();
+    cout << "\n-> ";
+    cin >> opsi;
+
+    if (opsi == "1")
+    {
+        kelas.pesanTiket();
+    }
+    else if (opsi == "2")
+    {
+        cout << "Program Selesai!" << endl;
+        return 0;
+    }
+    else 
+    {
+        cout << "Invalid Input!" << endl;
+    }
+    
+    
+    kelas.enterToContinue();
+    }
+akhir:
+    return 0;
+}
