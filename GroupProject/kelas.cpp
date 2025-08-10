99d22ee9b279cb0f7257ebc898de085fe3439b15
Radityo0o <agustinus.20246002@student.atmi.ac.id>
2025-08-10 22:43:17 +0700

    Update kelas.cpp

--- GroupProject/kelas.cpp
+++ GroupProject/kelas.cpp
@@ -1,7 +1,24 @@
 #include "kelas.h"
 #include <iostream>
 #include <iomanip>
+#include <fstream>
+#include <ctime>
 using namespace std;
+
+string getCurrentDateTime(){
+  time_t now = time(0);
+    tm* ltm = localtime(&now);
+
+    string date_time = to_string(1900 + ltm->tm_year) + "-" +
+        to_string(1 + ltm->tm_mon) + "-" +
+        to_string(ltm->tm_mday) + " " +
+        to_string(ltm->tm_hour) + ":" +
+        to_string(ltm->tm_min) + ":" +
+        to_string(ltm->tm_sec);
+
+    return date_time;
+  }
+
 
 void Kelass::clearScreen(){
     // Untuk Windows
@@ -71,6 +88,7 @@ void Kelass::pesanTiket(){
     case 3:
         namaKamar = "Suite";
         hargaKamar = 1200000;
+        break;
     default:
         namaKamar = "Invalid Input!";
         hargaKamar = 0;
@@ -88,5 +106,36 @@ void Kelass::pesanTiket(){
     cout << "Harga per Malam\t: Rp" << fixed << setprecision(0) << hargaKamar << endl;
     cout << "Jumlah Malam\t: " << jumlahMalam << endl;
     cout << "------------------------------" << endl;
-    cout << "Total Pembayaran: Rp" << fixed << setprecision(0) << totalBayar << endl; 
+    cout << "Total Pembayaran: Rp" << fixed << setprecision(0) << totalBayar << endl;
+
+    //Bagian Cetak 
+    char opsiCetak;
+    cout << "Cetak Kode Booking? (Y/N): "; 
+    cin >> opsiCetak;
+
+    if (opsiCetak == 'Y' || opsiCetak == 'y') {
+      ofstream notaFile("nota_hotel.txt");
+        if (notaFile.is_open()) {
+          notaFile << "========== NOTA PEMESANAN HOTEL ==========" << endl;
+          //notaFile << "Tanggal & Waktu: " << currentDateTime << endl;
+          notaFile << "------------------------------------------" << endl;
+          notaFile << "Nama Pemesan\t: " << namaPemesan << endl;
+          notaFile << "Kode Pemesan\t: " << kodePemesan << endl;
+          notaFile << "Jenis Kamar\t: " << namaKamar << endl;
+          notaFile << "Jumlah Malam\t: " << jumlahMalam << endl;
+          notaFile << fixed << setprecision(0);
+          notaFile << "Harga/malam\t: Rp" << hargaKamar << endl;
+          notaFile << "------------------------------------------" << endl;
+          notaFile << "Total Pembayaran: Rp" << totalBayar << endl;
+          notaFile << "==========================================" << endl;
+          notaFile.close();
+          cout << "Nota berhasil dicetak ke file nota_hotel.txt" << endl;
+        }
+        else {
+          cout << "Gagal membuka file nota_hotel.txt" << endl;
+         } 
+    } 
+    else if (opsiCetak == 'N' || opsiCetak == 'n') {
+      enterToContinue();
+    }
 }
