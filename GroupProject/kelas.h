981cef4dae617c024d5848083fb4ed2339df0ba2
Radityo0o <agustinus.20246002@student.atmi.ac.id>
2025-08-12 08:56:48 +0700

    Update kelas.h

--- GroupProject/kelas.h
+++ GroupProject/kelas.h
@@ -1,6 +1,19 @@
 #pragma once
 #include <string>
+#include <vector>
+#include <fstream>
 using namespace std;
+
+struct Booking {
+    string namaPemesan;
+    string kodePemesan;
+    string bookingDate;
+    int jumlahMalam;
+    string namaKamar;
+    double hargaKamar;
+    double totalBayar;
+};
+
 class Kelass {
 public:
     void clearScreen();
@@ -8,6 +21,19 @@ public:
     void headerUtama();
     void menuUtama();
     void pesanTiket();
+    void readBookings();
+    void updateBooking();
+    void deleteBooking();
     string getCurrentDateTime();
     bool isValidFutureDate(const string& date);
+    void saveToFile();
+    void loadFromFile();
+    void printNota(const Booking& booking);
+    void updateKamar(Booking& booking);
+    void updateTanggal(Booking& booking);
+    void updateJumlahMalam(Booking& booking);
+
+private:
+    vector<Booking> bookings;
+    const string filename = "bookings.txt";
 };
