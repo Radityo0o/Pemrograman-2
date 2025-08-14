1aa0982eb8974af93a70d0edfd9e317ef3673925
Radityo0o <agustinus.20246002@student.atmi.ac.id>
2025-08-14 13:03:48 +0700

    Update main.cpp

--- GroupProject/main.cpp
+++ GroupProject/main.cpp
@@ -1,23 +1,58 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include "kelas.h"
+#include "menupesan.h"
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 int main() {
     Kelass kelas;
+    string username, password;
+    char ch;
+    bool loginBerhasil = false;
 
     // Login system
-    string username, password;
-    kelas.clearScreen();
-    cout << "=== Login Sistem ===" << endl;
-    cout << "Username: ";
-    cin >> username;
-    cout << "Password: ";
-    cin >> password;
+    while (!loginBerhasil) {
+        kelas.clearScreen();
+        cout << "|=======================================|\n";
+        cout << "|                                       |\n";
+        cout << "|      Booking Hotel? Disini Aja        |\n";
+        cout << "|                                       |\n";
+        cout << "|=======================================|\n";
+        cout << endl;
+        cout << "=========      Login Sistem     =========" << endl;
+        cout << endl;
+        cout << "Silahkan Masukkan Username dan Password\n";
+        cout << "Username: ";
+        cin >> username;
 
-    if (username != "admin" || password != "admin123") {
-        cout << "Login gagal!" << endl;
-        return 0;
+        cout << "Password: ";
+        password = "";
+
+        while (true) {
+            ch = _getch(); // Ambil karakter tanpa menampilkannya
+
+            if (ch == '\r') { // Enter ditekan
+                break; 
+            }
+            else if (ch == '\b') { // Backspace ditekan
+                if (!password.empty()) {
+                    password.pop_back();
+                    cout << "\b \b"; // Hapus bintang dari layar
+                }
+            }
+            else {
+                password.push_back(ch);
+                cout << '*'; // Tampilkan bintang
+            }
+        }
+
+        if (username == "user" && password == "user123") {
+            loginBerhasil = true;
+        }
+        else {
+            cout << "\nLogin gagal! Silakan coba lagi.\n";
+            kelas.enterToContinue(); // Tunggu input sebelum ulang
+        }
     }
 
     // Load existing data
@@ -47,6 +82,12 @@ int main() {
             break;
         case 5:
             kelas.deleteBooking();
+            break;
+        case 6:
+            kelas.displayRoomStock();  // Menu baru: Lihat stok kamar
+            break;
+        case 7:
+            kelas.manageStock();       // Menu baru: Kelola stok (admin)
             break;
         default:
             cout << "Pilihan tidak valid!" << endl;
